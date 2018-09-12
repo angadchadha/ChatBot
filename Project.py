d904c23c9fe6e1d7f57e5d886fbaf08f99cbfb4e
Angad Chadha <41651471+angadchadha@users.noreply.github.com>
2018-09-12 12:47:13 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,47 +1,51 @@
 import spacy
 from spacy.symbols import nsubj, VERB
-import wikipediaapi
+import wikipedia
 
-intro = print("\nI am a bot who tells the date of the things.\nDon't forget to put 'when' before the question.\n ")
-while True:
-    h = ""
-    l1 = []
-    i = 0
-    o = 0
-    word = input("Ask me a question: ")
-    nlp  = spacy.load('en')
-    doc = nlp(word)
-    ncs = doc.noun_chunks
-    entities = doc.ents
-    sentences = doc.sents
+class ChatBot:
+    intro = print("\nI am a bot who tells the date of the things.\nDon't forget to put 'when' before the question.\n ")
+    def chat():
+        while True:
+            h = ""
+            l1 = []
+            i = 0
+            o = 0
+            word = input("Ask me a question: ")
+            nlp  = spacy.load('en')
+            doc = nlp(word)
+            ncs = doc.noun_chunks
+            entities = doc.ents
+            sentences = doc.sents
 
-    verbs = []
+            verbs = []
 
-    for n in ncs:
-            print("\nthe target subject is: ",n)
-            h = str(n).replace("the", "")
-    for n in sentences:
-        print("the question type: ",n[0])
-        print("the question details: ",n[1],n[5])
+            for n in ncs:
+                print("\nthe target subject is: ",n)
+                h = str(n).replace("the", "")
+
+            for n in doc:
+                if n.dep_ == "advmod":
+                    print ("the question type: ", n)
+                if n.dep_ == "nsubj" or n.dep_ == "ccomp":
+                    print ("the question details: ", n)
 
 
+            try:
+                page_py = wikipedia.page(h)
+                doc1 = nlp(page_py.summary)
+                entities1 = doc1.ents
 
+            except wikipedia.exceptions.PageError:
+                print("Wikipedia page does not exist! Try again.")
+                exit(1)
 
-    wiki_wiki = wikipediaapi.Wikipedia('en')
-
-    page_py = wiki_wiki.page(h)
-    doc1 = nlp(page_py.summary)
-    entities1 = doc1.ents
-    if page_py.exists():
-        for n in entities1:
+            oldest=0
+            for n in entities1:
                 if n.label_ == "DATE":
-                    i+=1
-                    if i < 5:
-                        l1 += n
-        if len(l1) > 1:
-            if l1[0] < l1[1]:
-                print (l1[0])
-        else:
-            print(l1[0])
-    else:
-        print("Sorry,I didn't understand the question.\nPlease make sure you have put 'when' before the question.")
+                    l1.append(n);
+                    if l1[i] < l1[oldest]:
+                        oldest=i
+
+            print(l1[oldest])
+    if __name__ == '__main__':
+        chat()
